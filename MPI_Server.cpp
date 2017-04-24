0499b6d9e8dbf6bf0922e03cd87177bb3b1380b3
techzbq <zbq_930713@163.com>
2017-04-24 21:28:31 +0800

    overwrite MPI_Base and Server

---
+++ MPI_Server.cpp
@@ -0,0 +1,332 @@
+//
+// Created by zhaobq on 2017-4-24.
+//
+
+#include "Include/MPI_Server.h"
+#include "Include/ErrorHandler.h"
+#include <string>
+#include <signal.h>
+
+#define DEBUG
+
+MPI_Server::MPI_Server(IRecv_buffer* rh, char *svc_name) : MPI_Base(rh) {
+    svc_name_ = svc_name;
+    recv_flag_mutex = PTHREAD_MUTEX_INITIALIZER;
+    accept_flag_mutex = PTHREAD_MUTEX_INITIALIZER;
+    comm_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+};
+
+MPI_Server::~MPI_Server() {
+    if(!recv_f && !accept_f)
+        stop();
+    cout << "[Server] end..." << endl;
+}
+
+int MPI_Server::initialize() {
+    cout << "--------------------Server init start-------------------" << endl;
+    int merr= 0;
+    int msglen = 0;
+    char errmsg[MPI_MAX_ERROR_STRING];
+
+    int provided;
+    MPI_Init_thread(0,0,MPI_THREAD_MULTIPLE, &provided);
+    cout << "[Server]: support thread level= " << provided << endl;
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Get_processor_name(hostname, &msglen);
+    //TODO set self costume handler, here set MPI_ERRORS_RETURN in temp
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+    cout << "[Server]: Host: " << hostname << ",Proc: "<< myrank << ", Server initialize..." << endl;
+    merr = MPI_Open_port(MPI_INFO_NULL, port);
+    if(merr){
+        MPI_Error_string(merr, errmsg, &msglen);
+        cout << "[Server]: Error in Open port :" << errmsg<<endl;
+        return MPI_ERR_CODE::OPEN_PORT_ERR;
+    }
+
+    cout << "[Server]: Host: " << hostname << ",Proc: "<< myrank << ",Server opening port on <" << port <<">" << endl;
+
+    merr = MPI_Publish_name(svc_name_, MPI_INFO_NULL, port);
+    if(merr){
+        MPI_Error_string(merr, errmsg, &msglen);
+        cout << "[Server]: Error in publish_name :" << errmsg<<endl;
+        return MPI_ERR_CODE::PUBLISH_SVC_ERR;
+    }
+    cout << "[Server]: publish service <" << svc_name_ << ">" << endl;
+
+    //start recv thread
+    pthread_create(&recv_t ,NULL, MPI_Base::recv_thread, this);
+    while(true){
+        if(!recv_f) {
+            break;
+        }
+    }
+    cout << "[Server]: receive thread start..." << endl;
+
+    //start accept thread
+    pthread_create(&pth_accept, NULL, MPI_Server::accept_conn_thread, this);
+    while(true){
+        if(!accept_f){
+            break;
+        }
+    }
+    cout << "[Server]: accept thread start..." << endl;
+
+    cout << "--------------------Server init finish--------------------" << endl;
+    return MPI_ERR_CODE::SUCCESS;
+}
+
+int MPI_Server::stop() {
+    cout << "--------------------Server stop start--------------------"<< endl;
+    int merr= 0;
+    int msglen = 0;
+    char errmsg[MPI_MAX_ERROR_STRING];
+    cout << "[Server]: Ready to stop..." << endl;
+    cout << "[Server]: Unpublish service name..." << endl;
+    merr = MPI_Unpublish_name(svc_name_, MPI_INFO_NULL, port);
+    if(merr){
+        MPI_Error_string(merr, errmsg, &msglen);
+        cout << "[Server]: Unpublish service name error: "<< errmsg << endl;
+        return MPI_ERR_CODE::UNPUBLISH_ERR;
+    }
+
+    if(!comm_map.empty()){
+        cout <<"[Server]: client still working ,cannot stop server..." << endl;
+        return MPI_ERR_CODE::STOP_ERR;
+    }
+    // TODO Add force stop
+    //stop threads
+    set_recv_stop();
+    if(finalize() != MPI_ERR_CODE::SUCCESS){
+        //TODO Do something for finalize error
+        return MPI_ERR_CODE::JOIN_THREAD_ERROR;
+    }
+    cout << "--------------------Server stop finish--------------------"  << endl;
+
+    return MPI_ERR_CODE::SUCCESS;
+}
+
+int MPI_Server::finalize() {
+    // set up a dummy connection to jump out of accept loop
+    int merr= 0;
+    int msglen = 0;
+    char errmsg[MPI_MAX_ERROR_STRING];
+    MPI_Comm tmp_comm;
+    dummy_conn = true;
+    merr = MPI_Comm_connect(port, MPI_INFO_NULL, 0, MPI_COMM_SELF, &tmp_comm);
+    if(merr){
+        MPI_Error_string(merr, errmsg, &msglen);
+        cout << "[Server-ERR]: Setup dummy connection error, msg: " << errmsg << endl;
+        return MPI_ERR_CODE::CONNECT_ERR;
+    }
+    MPI_Barrier(tmp_comm);
+    MPI_Comm_disconnect(&tmp_comm);
+#ifdef DEBUG
+    cout << "[Server]: Disconnect dummy connection, wait for accept thread stop" << endl;
+#endif
+    int ret;
+    ret = pthread_join(pth_accept, NULL);
+#ifdef DEBUG
+    cout << "[Server]: accept thread stop, exit code=" << ret << endl;
+#endif
+    if(ret != 0){
+        cout << "[Server-ERR]: Join **accept** thread error, code = " << ret << endl;
+        //TODO Do something
+        return MPI_ERR_CODE::JOIN_THREAD_ERROR;
+    }
+    ret = pthread_join(recv_t, NULL);
+#ifdef DEBUG
+    cout << "[Server]: recv_thread stop, exit code=" << ret << endl;
+#endif
+    if(ret != 0){
+        cout << "[Server-ERR]: Join **receive** thread error, code = " << ret << endl;
+        //TODO Do something
+        return MPI_ERR_CODE::JOIN_THREAD_ERROR;
+    }
+    MPI_Finalize();
+    return MPI_ERR_CODE::SUCCESS;
+}
+
+bool MPI_Server::new_msg_come(ARGS *args) {
+    //FIXME Can not fairly detect the last-half queue
+    if(comm_map.empty())
+        return false;
+    MPI_Status stat;
+    map<string, MPI_Comm>::iterator iter;
+    for(iter = comm_map.begin(); iter != comm_map.end(); iter++){
+        if(Recv_Probe(iter->second,&stat)) {
+#ifdef DEBUG
+            cout << "[Server]: dectect a new msg <source=" << stat.MPI_SOURCE << ";tag=" << stat.MPI_TAG << ">" <<endl;
+#endif
+            args->newcomm = (iter->second);
+            args->arg_stat = stat;
+            args->datatype = MPI_CHAR;
+            args->source_rank = stat.MPI_SOURCE;
+#ifdef DEBUG
+            args->print();
+#endif
+            return true;
+        }
+    }
+    return false;
+}
+
+void* MPI_Server::accept_conn_thread(void *ptr) {
+    //pthread_t mypid = pthread_self();
+    int merr= 0;
+    int msglen = 0;
+    char errmsg[MPI_MAX_ERROR_STRING];
+    pthread_mutex_lock(&(((MPI_Server*)ptr)->accept_flag_mutex));
+    ((MPI_Server*)ptr)->accept_f = false;
+    pthread_mutex_unlock(&(((MPI_Server*)ptr)->accept_flag_mutex));
+    cout << "[Server] host: "<< ((MPI_Server*)ptr)->hostname <<", accept connection thread start..." << endl;
+    int tmpkey = 0;
+    while(!((MPI_Server*)ptr)->accept_f) {
+        MPI_Comm newcomm;
+        merr = MPI_Comm_accept(((MPI_Server*)ptr)->port, MPI_INFO_NULL, 0, MPI_COMM_SELF, &newcomm);
+        if(merr){
+            MPI_Error_string(merr, errmsg, &msglen);
+            cout << "[Server-Error]: accept client error, msg: " << errmsg << endl;
+        }
+        MPI_Barrier(newcomm);
+        if(((MPI_Server*)ptr)->dummy_conn){
+            merr = MPI_Comm_disconnect(&newcomm);
+            if(merr){
+                MPI_Error_string(merr, errmsg, &msglen);
+                cout << "[Server-ERR]: <accept-thread> Disconnect dummy connection error, msg: " << errmsg << endl;
+            }
+            break;
+        }
+        pthread_mutex_lock(&(((MPI_Server*)ptr)->comm_list_mutex));
+        ((MPI_Server*)ptr)->allowstop = true;
+        while(((MPI_Server*)ptr)->comm_map[""+tmpkey] != NULL){
+            tmpkey++;
+        }
+        ((MPI_Server*)ptr)->comm_map.insert(pair<string, MPI_Comm>(""+tmpkey,newcomm));
+        tmpkey++;
+        pthread_mutex_unlock(&(((MPI_Server*)ptr)->comm_list_mutex));
+        //TODO receive worker MPI_REGISTEY tags and add to master, in recv_thread() function or ABC recv_commit() function
+#ifdef DEBUG
+        cout << "[Server]:Host: " << ((MPI_Server*)ptr)->hostname << ", Proc: "<< ((MPI_Server*)ptr)->myrank << ", receive new connection...; MPI_COMM="<< newcomm << endl;
+#endif
+        //TODO add to bcast_comm/group
+
+    }
+    cout << "[Server] host: "<< ((MPI_Server*)ptr)->hostname << ", accept connection thread stop..." << endl;
+    return 0;
+}
+
+void MPI_Server::recv_handle(ARGS args, void* buf) {
+    //TODO set different conditions
+    int merr, msglen;
+#ifdef DEBUG
+    cout << "[Server-DEBUG]: ARGS = " << endl;
+    args.print();
+#endif
+    string msg_tmp = (char *)buf;
+    string msg = msg_tmp.substr(0, (unsigned int) args.arg_stat.count);
+    char errmsg[MPI_MAX_ERROR_STRING];
+    map<string, MPI_Comm>::iterator iter;
+    switch(args.arg_stat.MPI_TAG){
+        case MPI_REGISTER: {
+#ifdef DEBUG
+            cout << "get a registery from worker:" << msg << endl;
+#endif
+            if(comm_map.size() == 0)
+                cout << "[Server-Error]: comm_list has no MPI_Comm" << endl;
+            int size = 0;
+            pthread_mutex_lock(&comm_list_mutex);
+            //modify comm_map
+            for(iter = comm_map.begin(); iter != comm_map.end(); iter++){
+                if(iter->second == args.newcomm && comm_map[msg] == NULL) {
+                    comm_map[msg] = iter->second;
+                    comm_map.erase(iter->first);
+#ifdef DEBUG
+                    cout << "[Server]: register worker " << msg << " success" << endl;
+#endif
+                    break;
+                }
+            }
+            if(iter == comm_map.end()){
+                cout << "[Server-Error]: register error, no compatible MPI_COMM" << endl;
+                //TODO Add error handle
+            }
+            else if(comm_map[msg] != NULL){
+                cout << "[Server-ERROR]: This uuid=" << msg << "has already registered" << endl;
+            }
+            pthread_mutex_unlock(&comm_list_mutex);
+        }
+            break;
+        case MPI_DISCONNECT:{
+
+            cout << "[Server] worker :" << msg<< " require disconnect" << endl;
+            pthread_mutex_lock(&comm_list_mutex);
+            if(comm_map[msg] != NULL){
+                if(comm_map[msg] == args.newcomm) {
+                    merr = MPI_Comm_disconnect(&comm_map[msg]);
+                    if (merr) {
+                        MPI_Error_string(merr, errmsg, &msglen);
+                        cout << "[Server-Error]: disconnect error: " << errmsg << endl;
+                    }
+                    comm_map.erase(msg);
+#ifdef DEBUG
+                    cout << "[Server]: find MPI_Comm and wid, removing worker..." << endl;
+#endif
+                }
+                else{
+                    cout << "[Server-ERR]: MPI_Comm can not match with MPI_Comm: <stored:" << comm_map[msg] << "> | <your:" << args.newcomm <<">" <<endl;
+                }
+            }
+            else{
+                // not found
+#ifdef DEBUG
+                cout << "[Server-Error]: can't find correspond MPI_Comm and wid" << endl;
+                cout << "Here is the comm list" << endl;
+                for(iter = comm_map.begin(); iter != comm_map.end(); iter++){
+                    cout << iter->first <<" :: " << iter->second << endl;
+                }
+#endif
+            }
+            pthread_mutex_unlock(&comm_list_mutex);
+        }
+            break;
+        default: {
+            cout << "[Server-Error]: unrecorgnized type" << endl;
+            break;
+        }
+    }
+}
+
+int MPI_Server::send_string(char *buf, int msgsize, string dest_uuid, int tag) {
+#ifdef DEBUG
+    cout << "[Server]: send message...<" << buf << ",msgsize="<< msgsize <<",dest="<<dest_uuid <<",tag=" <<tag  << ">"<< endl;
+#endif
+    int merr = 0;
+    int msglen = msgsize;
+    char errmsg[MPI_MAX_ERROR_STRING];
+    MPI_Comm send_comm = comm_map[dest_uuid];
+    if(send_comm == NULL) {
+#ifdef  DEBUG
+        cout << "[Server-Error]: can't find send comm" << endl;
+#endif
+        //TODO add error handler
+    }
+    merr = MPI_Send(buf, msgsize, MPI_CHAR, 0, tag, send_comm);
+    if(merr){
+        MPI_Error_string(merr, errmsg, &msglen);
+        cout << "[Server-Error]: send fail...error: " << errmsg << endl;
+        return MPI_ERR_CODE::SEND_FAIL;
+    }
+#ifdef DEBUG
+    cout << "[Server]: start barrier..." << endl;
+#endif
+    merr = MPI_Barrier(send_comm);
+    if(merr){
+        MPI_Error_string(merr, errmsg, &msglen);
+        cout << "[Server-Error]: barrier fail...error: " << errmsg << endl;
+        return MPI_ERR_CODE::BARRIER_FAIL;
+    }
+#ifdef DEBUG
+    cout << "[Server]: end barrier..." << endl;
+#endif
+    return MPI_ERR_CODE::SUCCESS;
+}
